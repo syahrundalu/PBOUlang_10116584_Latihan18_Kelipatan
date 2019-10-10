1f4bf08e309d5ef72af8149109251fd4abcb9cb6
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-10-10 16:04:33 +0700

    Create Kelipatan.java

---
+++ Kelipatan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatan;
+
+/**
+ *
+ * @author 
+ * NIM  : 10116584
+ * NAMA : Muhammad Syahrun Dalu
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a=0;
+        for (double b=1; b<= 10; b++){
+            a=b*3.5;
+            System.out.println(a);
+           
+            
+        }
+    }
+    
+}
